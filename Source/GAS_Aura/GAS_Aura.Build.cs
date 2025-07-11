ee5d86362f7c75b299ae45ca29c30b378947e313
zyx <zhangyuxin0216@outlook.com>
2025-07-11 23:30:46 +0800

    Aura Player Controller

--- Source/GAS_Aura/GAS_Aura.Build.cs
+++ Source/GAS_Aura/GAS_Aura.Build.cs
@@ -8,7 +8,13 @@ public class GAS_Aura : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore",
+			"EnhancedInput",
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- Source/GAS_Aura/Private/Player/AuraPlayerController.cpp
+++ Source/GAS_Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,34 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	// 响应服务器上变化的数据, 并发送到所有客户端
+	bReplicates = true;
+
+
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+
+	// 添加输入映射上下文
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+
+
+}
