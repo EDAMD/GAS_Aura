40a6138b2515b6c1dd0b31136f7db05c996318fc
zyx <zhangyuxin0216@outlook.com>
2025-07-12 00:37:33 +0800

    Movement Input

--- Source/GAS_Aura/Private/Player/AuraPlayerController.cpp
+++ Source/GAS_Aura/Private/Player/AuraPlayerController.cpp
@@ -3,18 +3,21 @@
 
 #include "Player/AuraPlayerController.h"
 #include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h"
+#include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h"
+#include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/InputActionValue.h"
+#include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/InputAction.h"
+
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	// 响应服务器上变化的数据, 并发送到所有客户端
 	bReplicates = true;
 
-
 }
 
 void AAuraPlayerController::BeginPlay()
 {
-	Super::BeginPlay();
+	Super::BeginPlay();	
 
 	check(AuraContext);
 
@@ -23,6 +26,7 @@ void AAuraPlayerController::BeginPlay()
 	check(Subsystem);
 	Subsystem->AddMappingContext(AuraContext, 0);
 
+	// 鼠标设置
 	bShowMouseCursor = true;
 	DefaultMouseCursor = EMouseCursor::Default;
 
@@ -31,5 +35,30 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
 
+	
+}
 
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation = FRotator(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
 }
