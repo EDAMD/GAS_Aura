6b44dc8794daebfbbbe35b6942b2aef99c3bbd30
zyx <zhangyuxin0216@outlook.com>
2025-07-12 12:01:41 +0800

    Game Mode

--- Source/GAS_Aura/Private/Character/AuraCharacter.cpp
+++ Source/GAS_Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,20 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+
+AAuraCharacter::AAuraCharacter()
+{    
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	
+	// 游戏开始时, 角色会自动被吸附在指定平面
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+}
