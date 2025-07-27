280b70af9e8be007d7c3ef0933a85780aaa14529
zyx <zhangyuxin0216@outlook.com>
2025-07-27 17:10:11 +0800

    Attack Behavior Tree Task

--- Source/GAS_Aura/Private/AI/AuraAIController.cpp
+++ Source/GAS_Aura/Private/AI/AuraAIController.cpp
@@ -7,7 +7,7 @@
 
 AAuraAIController::AAuraAIController()
 {
-	Blackboard = CreateDefaultSubobject<UBlackboardComponent>("BlackBoardComponent");
+	Blackboard = CreateDefaultSubobject<UBlackboardComponent>("BlackboardComponent");
 	check(Blackboard);
 	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>("BehaviorTreeComponent");
 	check(BehaviorTreeComponent);
