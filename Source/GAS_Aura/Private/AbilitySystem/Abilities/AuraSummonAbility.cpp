9dcc08c500e87c3d4651fcb01ae8616b856a8f8e
zyx <zhangyuxin0216@outlook.com>
2025-08-01 12:51:45 +0800

    Select Minion Class at Random

--- Source/GAS_Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
+++ Source/GAS_Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -2,4 +2,38 @@
 
 
 #include "AbilitySystem/Abilities/AuraSummonAbility.h"
+#include "Kismet/KismetSystemLibrary.h"
 
+TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
+{
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	
+	const float DeltaSpread = SpawnSpread / NumMinions;
+
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
+	const FVector RightOfSpread = Forward.RotateAngleAxis(SpawnSpread / 2.f, FVector::UpVector);
+	TArray<FVector> SpawnLocations;
+	for (int32 i = 0; i < NumMinions; i++)
+	{
+		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+		FVector ChosenLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+
+		// 使用射线追踪, 解决生成点如何在斜坡的问题
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(Hit, ChosenLocation + FVector(0.f, 0.f, 400.f), ChosenLocation - FVector(0.f, 0.f, 400.f), ECollisionChannel::ECC_Visibility);
+		if (Hit.bBlockingHit)
+		{
+			ChosenLocation = Hit.Location;
+		}
+		SpawnLocations.Add(ChosenLocation);
+	}
+
+	return SpawnLocations;
+}
+
+TSubclassOf<APawn> UAuraSummonAbility::GetRandomMinionClass()
+{
+	const int32 Selection = FMath::RandRange(0, MinionClasses.Num() - 1);
+	return MinionClasses[Selection];
+}
--- Source/GAS_Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
+++ Source/GAS_Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -14,4 +14,24 @@ class GAS_AURA_API UAuraSummonAbility : public UAuraGameplayAbility
 {
 	GENERATED_BODY()
 	
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocations();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	int32 NumMinions = 5;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MinSpawnDistance = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MaxSpawnDistance = 250.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float SpawnSpread = 90.f;
+
+	UFUNCTION(BlueprintPure, Category = "Summoning")
+	TSubclassOf<APawn> GetRandomMinionClass();
 };
