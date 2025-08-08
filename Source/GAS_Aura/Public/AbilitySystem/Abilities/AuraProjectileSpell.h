dec9e63021407e1c74d06b1ed0cca4e4f232fa27
zyx <zhangyuxin0216@outlook.com>
2025-08-08 12:04:46 +0800

    FireBolt Description

--- Source/GAS_Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
+++ Source/GAS_Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -16,6 +16,11 @@ class GAS_AURA_API UAuraProjectileSpell : public UAuraDamageGameplayAbility
 {
 	GENERATED_BODY()
 
+public:
+
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+
 protected:
 	virtual void ActivateAbility(
 		const FGameplayAbilitySpecHandle Handle, 
@@ -28,5 +33,8 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TSubclassOf<AAuraProjectile> ProjectileClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 NumProjectiles = 5;
 
 };
