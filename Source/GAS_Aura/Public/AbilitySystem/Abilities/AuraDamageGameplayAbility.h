56f6c67f8363627d5a01b4f4907c120df5b7a573
zyx <zhangyuxin0216@outlook.com>
2025-08-11 11:06:58 +0800

    Debuff Parameters

--- Source/GAS_Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
+++ Source/GAS_Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
@@ -20,11 +20,23 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TSubclassOf<UGameplayEffect> DamageEffectClass;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
 	FGameplayTag DamageType;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
 	FScalableFloat Damage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffChance = 20.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffDamage = 5.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffFrequency = 1.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffDuration = 5.f;
 
 	UFUNCTION(BlueprintCallable)
 	void CauseDamage(AActor* TargetActor);
--- Source/GAS_Aura/Public/AuraGameplayTags.h
+++ Source/GAS_Aura/Public/AuraGameplayTags.h
@@ -62,6 +62,11 @@ public:
 	FGameplayTag Debuff_Arcane;
 	FGameplayTag Debuff_Physical;
 
+	FGameplayTag Debuff_Chance;
+	FGameplayTag Debuff_Damage;
+	FGameplayTag Debuff_Frequency;
+	FGameplayTag Debuff_Duration;
+
 	FGameplayTag Effects_HitReact;
 
 	FGameplayTag Abilities_None;
