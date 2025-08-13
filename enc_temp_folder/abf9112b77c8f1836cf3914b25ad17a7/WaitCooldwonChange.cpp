816e50a04de36431e2ab96dc9056a2e7bc68ecb4
zyx <zhangyuxin0216@outlook.com>
2025-08-13 18:09:13 +0800

    Explode Dem FireBolts

---
+++ enc_temp_folder/abf9112b77c8f1836cf3914b25ad17a7/WaitCooldwonChange.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/AsyncTasks/WaitCooldwonChange.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayEffect.h"
+#include "ActiveGameplayEffectHandle.h"
+
+
+UWaitCooldwonChange* UWaitCooldwonChange::WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag InCooldownTag)
+{
+	UWaitCooldwonChange* WaitCooldownChange = NewObject<UWaitCooldwonChange>();
+	WaitCooldownChange->ASC = AbilitySystemComponent;
+	WaitCooldownChange->CooldownTag = InCooldownTag;
+
+	if (!IsValid(AbilitySystemComponent) || !InCooldownTag.IsValid())
+	{
+		WaitCooldownChange->EndTask();
+		return nullptr;
+	}
+
+	// 了解 CD Effect 什么时候被移除 (Cooldown Tag 被移除)
+	AbilitySystemComponent->RegisterGameplayTagEvent(InCooldownTag, EGameplayTagEventType::NewOrRemoved).AddUObject(
+		WaitCooldownChange,
+		&UWaitCooldwonChange::CooldownTagChanged);
+
+	// 了解 CD Effect 什么时候被应用
+	AbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(
+		WaitCooldownChange,
+		&UWaitCooldwonChange::OnActiveEffectAdded
+	);
+
+	return WaitCooldownChange;
+}
+
+void UWaitCooldwonChange::EndTask()
+{
+	if (!IsValid(ASC))
+	{
+		return;
+	}
+	ASC->RegisterGameplayTagEvent(CooldownTag, EGameplayTagEventType::NewOrRemoved).RemoveAll(this);
+
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UWaitCooldwonChange::CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount)
+{
+	if (NewCount == 0)
+	{
+		CooldownEnd.Broadcast(0.f);
+	}
+}
+
+void UWaitCooldwonChange::OnActiveEffectAdded(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	FGameplayTagContainer AssetTags;
+	SpecApplied.GetAllAssetTags(AssetTags);
+
+	FGameplayTagContainer GrantedTags;
+	SpecApplied.GetAllGrantedTags(GrantedTags);
+
+	if (AssetTags.HasTagExact(CooldownTag) || GrantedTags.HasTagExact(CooldownTag))
+	{
+		// 创建查询条件 (含有 CooldownTag 的 GE)
+		FGameplayEffectQuery GameplayEffectQuery = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(CooldownTag.GetSingleTagContainer());
+
+		// 使用上文创建的 查询条件 进行 查询 并返回值
+		TArray<float> TimesRemaining = ASC->GetActiveEffectsTimeRemaining(GameplayEffectQuery);
+		if (TimesRemaining.Num() > 0)
+		{
+			float TimeRemaining = TimesRemaining[0];
+			for (int32 i = 0; i < TimesRemaining.Num(); i++)
+			{
+				if (TimesRemaining[i] > TimeRemaining)
+				{
+					TimeRemaining = TimesRemaining[i];
+				}
+			}
+
+			CooldownStart.Broadcast(TimeRemaining);
+		}
+	}
+}
