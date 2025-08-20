ed24fde2cbf8cc2ce2509decf11694686682f3d2
zyx <zhangyuxin0216@outlook.com>
2025-08-20 18:31:12 +0800

    Checkpoints

---
+++ Source/GAS_Aura/Private/CheckPoint/CheckPoint.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CheckPoint/CheckPoint.h" 
+#include "Components/SphereComponent.h"
+
+
+ACheckPoint::ACheckPoint(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{  
+    PrimaryActorTick.bCanEverTick = true;  
+
+	CheckPointMesh = CreateDefaultSubobject<UStaticMeshComponent>("CheckPointMesh");
+	CheckPointMesh->SetupAttachment(GetRootComponent());
+	CheckPointMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CheckPointMesh->SetCollisionResponseToAllChannels(ECR_Block);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(CheckPointMesh);
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void ACheckPoint::BeginPlay()
+{
+    Sphere->OnComponentBeginOverlap.AddDynamic(this, &ACheckPoint::OnSphereOverlap);
+}
+
+void ACheckPoint::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag(FName("Player")))
+	{
+		HandleGlowEffects();
+	}
+}
+
+void ACheckPoint::HandleGlowEffects()
+{
+	Sphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	UMaterialInstanceDynamic* DynamicMaterialInstance = UMaterialInstanceDynamic::Create(CheckPointMesh->GetMaterial(0), this);
+	CheckPointMesh->SetMaterial(0, DynamicMaterialInstance);
+	CheckPointReached(DynamicMaterialInstance);
+}
